0e8162cca32fff552ed58a688693979ee0e3688f
Raymond Chung <raymond_chung2@hotmail.com>
2016-02-06 14:11:17 -0500

    Added functionality from newLogs to logs

--- app/src/main/java/com/enochtam/decklog/newLogs.java
+++ app/src/main/java/com/enochtam/decklog/newLogs.java
@@ -1,8 +1,11 @@
 package com.enochtam.decklog;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class newLogs extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class newLogs extends AppCompatActivity {
         setContentView(R.layout.activity_new_logs);
         assert getSupportActionBar() != null;
         getSupportActionBar().setTitle("New Log");
+    }
+
+    public void buttonOnClick(View v) {
+        Intent i = new Intent(this,logs.class);
+        startActivity(i);
     }
 }
