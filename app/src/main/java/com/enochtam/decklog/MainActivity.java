2afe5ede36b4767c3a700d0e329b0bbaa518708d
Ryan Baxter <ryan.baxter@queensu.ca>
2016-02-06 15:24:11 -0500

    Main Activity arrays

--- app/src/main/java/com/enochtam/decklog/MainActivity.java
+++ app/src/main/java/com/enochtam/decklog/MainActivity.java
@@ -1,29 +1,52 @@
 package com.enochtam.decklog;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
+    private DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Intent i = new Intent(this,newLogs.class);
+        //startActivity(i);
 
-        DBHelper dbHelper = new DBHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db = new DBHelper(getApplicationContext());
 
-        Intent i = new Intent(this,newLogs.class);
-        startActivity(i);
+        //db.insertLogs("Hello","Test","Test2");
 
+        arrayList = db.getAllLogs();
 
         assert getSupportActionBar() != null;
         ActionBar ab = getSupportActionBar();
         ab.setTitle(getString(R.string.main_activity_title));
+
+        ListView list = (ListView) findViewById(R.id.log_list);
+        adapter=new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                arrayList);
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "my position is " + position + " and my id is " + id, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
-
-
