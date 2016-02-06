ceb275bab39bf9113b4d5728ad9c406d88e87a28
Enoch Tam <et.enoch@gmail.com>
2016-02-06 14:22:14 -0500

    Fixed db create

--- app/src/main/java/com/enochtam/decklog/MainActivity.java
+++ app/src/main/java/com/enochtam/decklog/MainActivity.java
@@ -1,6 +1,7 @@
 package com.enochtam.decklog;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
         Intent i = new Intent(this,newLogs.class);
         startActivity(i);
 
