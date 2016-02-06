a0d7d0b2f5542f76a6d24320530e684ee7095949
Raymond Chung <raymond_chung2@hotmail.com>
2016-02-06 13:04:32 -0500

    Adding new log file

---
+++ app/src/main/java/com/enochtam/decklog/newLogs.java
@@ -0,0 +1,13 @@
+package com.enochtam.decklog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class newLogs extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_logs);
+    }
+}
